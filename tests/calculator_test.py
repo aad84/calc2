861a584b47d2f3d6cca5c70ac53e457390f77561
aad84 <90499269+aad84@users.noreply.github.com>
2021-11-11 01:54:22 +0530

    Task: Fixed division code zero exception

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,49 +1,39 @@
 """Testing the Calculator"""
-# disabling:
-# C0304: Trailing newlines (trailing-newlines)
-# pylint: disable=C0304
-import pytest
 from calculator.main import Calculator
 
-
-def test_calculator_result():
+def test_calculator_add_result():
     """testing calculator result is 0"""
     calc = Calculator()
-    assert calc.result == 0
+    result = calc.add(1,2)
+    assert result == 3
 
-def test_zero_division():
-    """Testing the division function of the calculator"""
+def test_calculator_subtract_result():
+    """testing calculator result is 0"""
     calc = Calculator()
-    with pytest.raises(ZeroDivisionError):
-        calc.division_numbers(1,0)
-def test_calculator_add():
-    """Testing the Add function of the calculator"""
-    #AAA-Arrange by instantiating the calc class
-    calc = Calculator()
-    #AAA-Act by calling the method to be tested
-    calc.add_number(4)
-    #AAA-Assert that the results are correct
-    assert calc.result == 4
+    result = calc.subtract(1,2)
+    assert result == -1
 
-def test_calculator_get_result():
-    """Testing the Get result method of the calculator"""
-    calc = Calculator()
-    assert calc.get_result() == 0
 
-def test_calculator_subtract():
-    """Testing the subtract method of the calculator"""
+def test_calculator_multiply_result():
+    """testing calculator result is 0"""
     calc = Calculator()
-    calc.subtract_number(1)
-    assert calc.get_result() == -1
-def test_calculator_multiply():
-    """ tests multiplication of two numbers"""
-    calc = Calculator()
-    result  = calc.multiply_numbers(1,2)
-    assert result == 2
-def test_calculator_division():
-    """ tests division of two numbers"""
-    calc = Calculator()
-    result = calc.division_numbers(2,1)
+    result = calc.multiply(1,2)
     assert result == 2
 
+def test_calculator_divide_result():
+    """testing calculator result is 0"""
+    calc = Calculator()
+    result = calc.divide(1,2)
+    assert result == 0.5
 
+def test_calculator_divide_fail_result():
+    """testing calculator result is 0"""
+    calc = Calculator()
+    result = calc.divide(1,0)
+    assert result == "Error: A number cannot be divided by 0"
+
+def test_calculator_increment_result():
+    """testing calculator result is 0"""
+    calc = Calculator()
+    result = calc.increment(2)
+    assert result == 3

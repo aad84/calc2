0601383cdd86e1d7996232084728f53f46efbdea
aad84 <90499269+aad84@users.noreply.github.com>
2021-11-11 02:14:14 +0530

    Fixed division and zero exception

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,39 +1,37 @@
 """Testing the Calculator"""
 from calculator.main import Calculator
 
-def test_calculator_add_result():
+def test_calculator_result():
     """testing calculator result is 0"""
     calc = Calculator()
-    result = calc.add(1,2)
-    assert result == 3
+    assert calc.result == 0
 
-def test_calculator_subtract_result():
-    """testing calculator result is 0"""
+def test_calculator_add():
+    """Testing the Add function of the calculator"""
+    #Arrange by instantiating the calc class
     calc = Calculator()
-    result = calc.subtract(1,2)
-    assert result == -1
+    #Act by calling the method to be tested
+    calc.add_number(4)
+    #Assert that the results are correct
+    assert calc.result == 4
 
-
-def test_calculator_multiply_result():
-    """testing calculator result is 0"""
+def test_calculator_get_result():
+    """Testing the Get result method of the calculator"""
     calc = Calculator()
-    result = calc.multiply(1,2)
+    assert calc.get_result() == 0
+
+def test_calculator_subtract():
+    """Testing the subtract method of the calculator"""
+    calc = Calculator()
+    calc.subtract_number(1)
+    assert calc.get_result() == -1
+def test_calculator_multiply():
+    """ tests multiplication of two numbers"""
+    calc = Calculator()
+    result  = calc.multiply_numbers(1,2)
     assert result == 2
-
-def test_calculator_divide_result():
-    """testing calculator result is 0"""
+def test_calculator_divide():
+    """ tests division of two numbers"""
     calc = Calculator()
-    result = calc.divide(1,2)
-    assert result == 0.5
-
-def test_calculator_divide_fail_result():
-    """testing calculator result is 0"""
-    calc = Calculator()
-    result = calc.divide(1,0)
-    assert result == "Error: A number cannot be divided by 0"
-
-def test_calculator_increment_result():
-    """testing calculator result is 0"""
-    calc = Calculator()
-    result = calc.increment(2)
-    assert result == 3
+    result  = calc.divide_numbers(2,1)
+    assert result == 2

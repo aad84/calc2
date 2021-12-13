0ff042b7c779d9e356cd21f50b103db3f6ac8563
aad84 <90499269+aad84@users.noreply.github.com>
2021-12-13 21:29:21 +0530

    Edit: main file

--- calculator/main.py
+++ calculator/main.py
@@ -1,39 +1,39 @@
 import pandas as pd
 import logging
 
-#filename = "Addition.csv - Sheet1.csv"
+#filename = "Addition.csv"
 #df = pd.read_csv(filename)
 #df.head(2)
 #df.columns
 
 
 class Calculator:
-    df = pd.read_csv("testdata/Addition.csv- Sheet1.csv")
-    filename="Addition.csv - Sheet1.csv"
+    df = pd.read_csv("testdata/Addition.csv")
+    filename="Addition.csv"
 
 
     def addition(df):
-        df['Addition']= df['Number 1'] + df['Number 2']
-        filename = "Addition.csv - Sheet1.csv"
+        df['Addition']= df['Number1'] + df['Number2']
+        filename = "Addition.csv"
         logging.info("Addition Complete for Input file {} and the result is {}".format(filename,df["Addition"]))
         return df
 
     def subtraction(df):
-        df['Subtraction'] = df['Number 1'] - df['Number 2']
-        filename = "Subtraction.csv - Sheet1.csv"
+        df['Subtraction'] = df['Number1'] - df['Number2']
+        filename = "Subtraction.csv"
         logging.info("Subtraction Complete for Input file {}".format(filename))
         return df
 
     def multiplication(df):
-            df['Multiplication'] = df['Number 1'] * df['Number 2']
-            filename = "Multiplication.csv - Sheet1.csv"
+            df['Multiplication'] = df['Number1'] * df['Number2']
+            filename = "Multiplication.csv"
             logging.info("Multiplication Complete for Input file {}".format(filename))
             return df
 
     def division(df):
 
-                df['Division'] = df['Number 1'] / df['Number 2']
-                filename = "Division.csv - Sheet1.csv"
+                df['Division'] = df['Number1'] / df['Number2']
+                filename = "Division.csv"
                 logging.info("Division Complete for Input file {}".format(filename))
                 return df
 
